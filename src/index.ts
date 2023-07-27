214e49568238e0824885dd1f8871dbd267e016cc
MiaAntunes <suportecamilaantunes2003@hotmail.com>
2023-07-27 11:26:31 -0300

    FEAT: implementacao typescript express knex e sql

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import  express, { Request, Response} from 'express'
+
+//import do CORS 👇🏽
+import cors from 'cors';
+
+//criação do servidor express 👇🏽
+const app = express();
+
+//configuração do middleware que garante que nossas respostas estejam sempre
+//no formato json 👇🏽
+app.use(express.json());
+
+//configuração do middleware que habilita o CORS 👇🏽
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
